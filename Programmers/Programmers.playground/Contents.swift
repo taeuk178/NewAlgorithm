f058a7929f1ea406a8c8aad78999adea8d4ca665
taeuk <nous178@naver.com>
2021-10-27 16:40:02 +0900

    level_1 최소직사각형

--- Programmers/Programmers.playground/Contents.swift
+++ Programmers/Programmers.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
 
+func solution(_ sizes:[[Int]]) -> Int {
+    
+    var newSizes = sizes
+    var widthMax: Int = 0
+    var heightMax: Int = 0
+    
+    for i in 0..<newSizes.count {
+        
+        if newSizes[i][0] < newSizes[i][1] {
+            newSizes[i].reverse()
+        }
+        
+        if widthMax < newSizes[i][0] {
+            widthMax = newSizes[i][0]
+        }
+        
+        if heightMax < newSizes[i][1] {
+            heightMax = newSizes[i][1]
+        }
+    }
+    return widthMax * heightMax
+}
+
+print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
+//print(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]))
+
